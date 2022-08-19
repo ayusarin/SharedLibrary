271638b4e0a8aa5df025d71c21ac062ee4877ba5
ayusarin <107953219+ayusarin@users.noreply.github.com>
2022-08-19 16:00:26 -0400

    Create helloWorld.groovy

---
+++ vars/helloWorld.groovy
@@ -0,0 +1,7 @@
+def call() {
+  // call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    //echo "Hello, ${name}."
+    sh "echo hello world"
+}
