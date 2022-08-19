aa7f03d12a18ad2a48982b3410e323f2dbca8322
ayusarin <107953219+ayusarin@users.noreply.github.com>
2022-08-19 16:03:51 -0400

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,3 @@
-def call() {
-  // call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    //echo "Hello, ${name}."
-    sh "echo hello world"
+def call(String name,String DayOfWeek) {
+  //echo "Hello, ${name}.Today is ${DayOfWeek}"
 }
